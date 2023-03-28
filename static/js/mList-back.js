b7572b2472e779c79bf268c3b760b3c6ce128f41
corsair-hs <hsk.computer.1@gmail.com>
2023-03-29 06:43:22 +0900

    css, js 외부참조로 변경

---
+++ static/js/mList-back.js
@@ -0,0 +1,75 @@
+function mList_select(imgValue) {
+    const _imgValue = imgValue;
+    fetch('/mList_select_GET').then((res) => res.json()).then((data) => {
+        let rows = data['result']
+        $("#cards-box").empty()
+        rows.forEach((a) => {
+            let weather = a["weather"]
+            let name = a["name"]
+            let url = a["url"]
+            let title = a["title"]
+            let desc = a["desc"]
+            let thumb = a['thumb']
+            let ytbId = a['ytbId']
+
+            if (weather === _imgValue) {
+                let temp_html = `<div class="col">
+                                        <div class="card h-100">
+                                            <img src="${thumb}"
+                                                class="card-img-top">
+                                            <div class="card-body">
+                                                <h5 class="card-title">${title}</h5>
+                                                <p class="card-text">${desc}</p>
+                                                <p class="card-text ytbId">${ytbId}</p>
+                                                <p class="mycomment">${name}</p>
+                                                <button type="button" onclick="update_music(this)" class="btn btn-outline-dark btn-sm">수정</button>
+                                                <button type="button" onclick="mList_delete(this)" class="btn btn-outline-dark btn-sm">삭제</button>
+                                            </div>
+                                        </div>
+                                    </div>`
+                $("#cards-box").append(temp_html);
+            }
+        })
+    });
+}
+
+function mList_delete(button) {
+    let row = $(button).closest('div.card-body');
+    let ytbId = row.find('p:eq(1)').text();
+    let weather = $("#weatherLabel").text();
+    // console.log(ytbId)
+
+    let formData = new FormData();
+    formData.append("ytbId_give", ytbId);
+    fetch('/mList_delete_POST', { method: "POST", body: formData, }).then((res) => res.json()).then((data) => {
+        alert(data["msg"]);
+        mList_select(weather)
+    });
+};
+
+function mList_insert() {
+    let weather = $("#weatherLabel").text();
+    let name = $("#name").val();
+    let url = $("#url").val();
+    let desc = $("#desc").val();
+
+    // desc 해시태그로 변환해주는 코드
+    let arrDesc = desc.split(' ');   // ' ' 공백을 구분자로 arrDesc 배열에 담기
+    let descResult = arrDesc.map((value) => '#'+value).join(' ');   // arrDesc 배열을 순회하며 각 요소들 앞에 #붙이기
+                                                                                  // arrDesc 배열의 모든 요소들을 ' ' 공백을 사이로 하나의 문자열로 조인시키기
+
+    let formData = new FormData();
+    formData.append("weather_give", weather)
+    formData.append("name_give", name)
+    formData.append("url_give", url)
+    formData.append("desc_give", descResult)
+
+    fetch('/mList_insert_POST', { method: "POST", body: formData, }).then((response) => response.json()).then((data) => {
+        alert(data["msg"]);
+        $("#name").val('')
+        $("#url").val('')
+        $("#desc").val('')
+        close_box();
+        mList_select(weather)
+    });
+}
---
+++ static/js/mList-front.js
@@ -0,0 +1,62 @@
+$(document).ready(function () {
+    hidebtnMusicInsert();
+});
+
+function mytitleEdit() {
+    $('div.mytitle').css('height', '350px');
+    $('p#weatherQuestion').css('font-size', '1.5em')
+    $('p#weatherQuestion').css('margin-bottom', '20px')
+    $('div#weatherWrap > img').css('width', '80px')
+    $('div#weatherBottom').hide();
+    showbtnMusicInsert()
+}
+function weatherImgToGray() {
+    $('#imgSunny').css('content', "url('../static/src/img/gray-sunny.png')")
+    $('#imgCloudy').css('content', "url('../static/src/img/gray-cloudy.png')")
+    $('#imgRainy').css('content', "url('../static/src/img/gray-rainy.png')")
+    $('#imgSnow').css('content', "url('../static/src/img/gray-snow.png')")
+}
+function sunnyClick() {
+    mytitleEdit();
+    weatherImgToGray();
+    $('#imgSunny').css('content', "url('../static/src/img/color-sunny.png')");
+    mList_select('sunny');
+    $('#weatherLabel').text('sunny');
+}
+function cloudyClick() {
+    mytitleEdit();
+    weatherImgToGray();
+    $('#imgCloudy').css('content', "url('../static/src/img/color-cloudy.png')");
+    mList_select('cloudy');
+    $('#weatherLabel').text('cloudy');
+}
+function rainyClick() {
+    mytitleEdit();
+    weatherImgToGray();
+    $('#imgRainy').css('content', "url('../static/src/img/color-rainy.png')");
+    mList_select('rainy');
+    $('#weatherLabel').text('rainy');
+}
+function snowClick() {
+    mytitleEdit();
+    weatherImgToGray();
+    $('#imgSnow').css('content', "url('../static/src/img/color-snow.png')");
+    mList_select('snow');
+    $('#weatherLabel').text('snow');
+}
+
+function showbtnMusicInsert() {
+    $('#btnMusicInsert').show();
+}
+
+function hidebtnMusicInsert() {
+    $('#btnMusicInsert').hide();
+}
+
+function open_box() {
+    $('#post-box').show()
+}
+
+function close_box() {
+    $('#post-box').hide()
+}
